fb650b948d369fdf71655903cb29b3c311db46c3
Pozniak17 <85741218+Pozniak17@users.noreply.github.com>
2024-02-08 20:15:29 +0000

    Deploying to gh-pages from @ Pozniak17/goit-react-hw-05-movies@4b94cf2d119f02f4110a521d05ab4a21ed684bef 🚀

---
+++ static/js/794.b10cfac6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[794],{794:function(e,t,r){r.r(t);var o=r(87),a=r(184);t.default=function(){return(0,a.jsxs)("div",{children:["Not Found Page! Go ",(0,a.jsx)(o.rU,{to:"/",children:"Home Page"})]})}}}]);
+//# sourceMappingURL=794.b10cfac6.chunk.js.map
